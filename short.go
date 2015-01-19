a294055154e665269109a80e61ccf2f7e24b541e
Phillip Ridlen <phillip@ovenbits.com>
2015-01-19 10:42:11 -0600

    Clean up router

--- short.go
+++ short.go
@@ -4,19 +4,19 @@ import (
   "crypto/rand"
   "encoding/hex"
   "fmt"
+  "github.com/garyburd/redigo/redis"
   "github.com/gorilla/mux"
+  "github.com/soveran/redisurl"
   "net/http"
   "os"
-  "github.com/soveran/redisurl"
-  "github.com/garyburd/redigo/redis"
 )
 
 func main() {
-  r := mux.NewRouter()
-  r.HandleFunc("/", short).Methods("POST")
-  r.HandleFunc("/{code}", redir)
+  route := mux.NewRouter()
+  route.HandleFunc("/{code}", RedirectToLongURL).Methods("GET")
+  route.HandleFunc("/", MakeShortURL).Methods("POST")
 
-  http.Handle("/", r)
+  http.Handle("/", route)
 
   fmt.Println("Listening...")
   err := http.ListenAndServe(":" + os.Getenv("PORT"), nil)
@@ -25,27 +25,22 @@ func main() {
   }
 }
 
-func redir(res http.ResponseWriter, req *http.Request) {
-  code := req.URL.Path[1:]
+func MakeShortURL(res http.ResponseWriter, req *http.Request) {
+  longUrl := req.FormValue("url")
+  code, _ := randomHex(5)
+  client, _ := redisurl.ConnectToURL(os.Getenv("REDISCLOUD_URL"))
+  client.Do("SET", code, longUrl)
+  client.Close()
+  fmt.Fprintln(res, os.Getenv("HOST") + "/" + code)
+}
 
+func RedirectToLongURL(res http.ResponseWriter, req *http.Request) {
+  code := req.URL.Path[1:]
   client, _ := redisurl.ConnectToURL(os.Getenv("REDISCLOUD_URL"))
   longUrl, _ := redis.String(client.Do("GET", code))
   client.Close()
 
   http.Redirect(res, req, longUrl, 302)
-}
-
-func short(res http.ResponseWriter, req *http.Request) {
-  longUrl := req.FormValue("url")
-  fmt.Fprintln(res, makeShort(longUrl))
-}
-
-func makeShort(longUrl string) (string) {
-  code, _ := randomHex(5)
-  client, _ := redisurl.ConnectToURL(os.Getenv("REDISCLOUD_URL"))
-  client.Do("SET", code, longUrl)
-  client.Close()
-  return os.Getenv("HOST") + "/" + code
 }
 
 func randomHex(n int) (string, error) {
@@ -55,5 +50,4 @@ func randomHex(n int) (string, error) {
   }
   return hex.EncodeToString(bytes), nil
 }
-
 
