15e2c6a4df747debbb53780886d6464958342fe4
Phillip Ridlen <phillip@ovenbits.com>
2015-01-18 22:04:41 -0600

    URL shortener

--- short.go
+++ short.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+  "crypto/rand"
+  "encoding/hex"
   "fmt"
+  "github.com/fzzy/radix/redis"
+  "github.com/gorilla/mux"
   "net/http"
   "os"
 )
 
 func main() {
-  http.HandleFunc("/", hello)
-  fmt.Println("listening...")
+  r := mux.NewRouter()
+  r.HandleFunc("/", short).Methods("POST")
+  r.HandleFunc("/{code}", redir)
+
+  http.Handle("/", r)
+
+  fmt.Println("Listening...")
   err := http.ListenAndServe(":" + os.Getenv("PORT"), nil)
   if err != nil {
     panic(err)
   }
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(res, "Hi!")
+func redir(res http.ResponseWriter, req *http.Request) {
+  code := req.URL.Path[1:]
+  client, _ := redis.Dial("tcp", os.Getenv("REDISCLOUD_URL"))
+  longUrl, _ := client.Cmd("GET", code).Str()
+  http.Redirect(res, req, longUrl, 302)
 }
+
+func short(res http.ResponseWriter, req *http.Request) {
+  longUrl := req.FormValue("url")
+  fmt.Fprintln(res, makeShort(longUrl))
+}
+
+func makeShort(longUrl string) (string) {
+  code, _ := randomHex(5)
+  client, _ := redis.Dial("tcp", os.Getenv("REDISCLOUD_URL"))
+  client.Cmd("SET", code, longUrl)
+  return os.Getenv("HOST") + "/" + code
+}
+
+func randomHex(n int) (string, error) {
+  bytes := make([]byte, n)
+  if _, err := rand.Read(bytes); err != nil {
+    return "", err
+  }
+  return hex.EncodeToString(bytes), nil
+}
+
 
